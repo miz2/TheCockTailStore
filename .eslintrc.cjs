083f900f706618b90eec806a58fda47d5e941af5
Mizaan ur Rehman <91748810+miz2@users.noreply.github.com>
2024-10-21 13:50:46 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,20 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'eslint:recommended',  // Basic ESLint rules
+    'plugin:react/recommended',  // React recommended rules
+    'plugin:react/jsx-runtime',  // Support for React's JSX transform
+    'plugin:react-hooks/recommended'  // React hooks specific linting rules
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],  // Ignore the build folder
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: { react: { version: 'detect' } },  // Automatically detect the installed React version
+  plugins: ['react-refresh'],  // Plugin for React Fast Refresh
   rules: {
-    'react/jsx-no-target-blank': 'off',
+    'react/jsx-no-target-blank': 'off',  // Disable no target blank rule
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+      { allowConstantExport: true }
+    ]
+  }
+};
